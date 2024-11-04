e62fcb75d0ed8d3953f69060df1f4a97dfc9b6eb
Javier Simbaña <sjavi18_alt@hotmail.com>
2024-11-04 05:19:40 +0000

    Copilot second commit

---
+++ .devcontainer/member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+  return [
+    {
+      name: 'HTML',
+      level: 80
+    },
+    {
+      name: 'CSS',
+      level: 70
+    },
+    {
+      name: 'JavaScript',
+      level: 65
+    },
+    {
+      name: 'React',
+      level: 60
+    },
+    {
+      name: 'Node.js',
+      level: 50
+    }
+  ];
+}
